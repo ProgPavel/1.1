b1034f52df554cae642c9f98c5b24c4d3777c283
Павел Иванов <pavel2708@ukr.net>
2023-12-06 19:31:40 +0200

    qqqqq

---
+++ FileName.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+    public struct ModelStruct
+    {
+        int r;
+        int y;
+        int р;
+        public int x;
+    }
+
+    class ModelClass
+    {
+        public int x;      
+    }
+                  
+    static class MyFunctions
+    {          
+        public static void ProcessStruct(ModelStruct modelStruct)
+        {
+             modelStruct.x = 10;
+        }
+        public static void ProcessClass(ModelClass modelClass)
+        {
+            modelClass.x = 10;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,75 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//вввв
-//вввв
+
+
+
+using ConsoleApp1;
+
+//Заняття 2.1
+//Заняття 2.1
+//11111111111
+//11111111111
+//222222222//11111111111
+//222222222
+//11111111111
+//222222222//11111111111
+//222222222
+//222222222ccccccccc
+//string Hello = "Hello, World!";
+//int x = 10;
+
+//Console.WriteLine(x);
+
+//ModelStruct modelStruct = new ModelStruct
+//{
+//    x = 5
+//};
+
+//ModelClass modelclass = new ModelClass
+//{
+//    x = 5
+//};
+
+//Console.WriteLine(modelStruct.x);
+//Console.WriteLine(modelclass.x);
+//Console.ReadKey(true);
+
+//MyFunctions.ProcessStruct(modelStruct);
+//MyFunctions.ProcessClass(modelclass);
+
+//Console.WriteLine(modelStruct.x);
+//Console.WriteLine(modelclass.x);
+
+
+//bool f = new();
+//int i = new();
+//var d = new double();
+//Console.WriteLine("{0},{1},{2}", f, i, d);
+
+double hyp = 4.0;
+Console.WriteLine("{0:#.###}.", hyp);
+
+bool logic ;
+
+logic = false;
+
+if (9 < 10) 
+{
+    logic = true;
+}
+
+Console.WriteLine("9<10 is" + logic);
+
+Console.WriteLine("bool.FalseString: {0}", bool.FalseString);
+Console.WriteLine("bool.TrueString: {0}", bool.TrueString);
+
+//Проверка на null
+double? d3 = null;
+if (d3.HasValue)
+{
+    Console.WriteLine("d3.HasValue" + d3.HasValue);
+}
+else
+{
+    Console.WriteLine("d3 іs null");
+}
+
